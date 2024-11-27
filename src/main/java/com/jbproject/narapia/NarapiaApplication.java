ac1eca0c1f1e667f5813d01a82b082bcc12b1f0e
Byungwook <byungwook91.jeon@lgepartner.com>
2024-11-28 08:47:31 +0900

    init commit

---
+++ src/main/java/com/jbproject/narapia/NarapiaApplication.java
@@ -0,0 +1,13 @@
+package com.jbproject.narapia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarapiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarapiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbproject/narapia/NarapiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbproject.narapia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarapiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
