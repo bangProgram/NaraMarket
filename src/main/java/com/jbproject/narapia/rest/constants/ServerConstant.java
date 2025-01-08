89ad410fd7fcb90c8c42ffc1d30eca38f49a52c7
bangProgram <40655921+bangProgram@users.noreply.github.com>
2025-01-08 22:57:53 +0900

    api url 및 path 변경

--- src/main/java/com/jbproject/narapia/rest/constants/ServerConstant.java
+++ src/main/java/com/jbproject/narapia/rest/constants/ServerConstant.java
@@ -5,13 +5,13 @@ public class ServerConstant {
     public final static long NARA_DELAY_MILLI_SEC = 5 * 1000;
     public final static Long SEARCH_NUM_OF_ROWS = 100L;
     public final static int WINBID_NUM_OF_ROWS = 999;
-    public final static String NARA_MAIN_URL = "https://apis.data.go.kr/1230000";
-    public final static String NARA_BIDNOTI_PATH = "/BidPublicInfoService05";
-    public final static String BIDNOTI_GOODS_METHOD = "/getBidPblancListInfoThng02";
+    public final static String NARA_MAIN_URL = "https://apis.data.go.kr/1230000/ad";
+    public final static String NARA_BIDNOTI_PATH = "/BidPublicInfoService";
+    public final static String BIDNOTI_GOODS_METHOD = "/getBidPblancListInfoThng";
     public final static String BIDNOTI_CONSTRUCTION_PATH = "/getBidPblancListInfoCnstwk02";
     public final static String BIDNOTI_FORG_CAPITAL_PATH = "/getBidPblancListInfoFrgcpt02";
     public final static String BIDNOTI_SERVICE_PATH = "/getBidPblancListInfoServc02";
-    public final static String BIDNOTI_GOODS_DETAIL_METHOD = "/getBidPblancListInfoThngBsisAmount02";
+    public final static String BIDNOTI_GOODS_DETAIL_METHOD = "/getBidPblancListInfoThngBsisAmount";
     public final static String NARA_WINBID_PATH = "/ScsbidInfoService01";
     public final static String WINBID_PATH = "/ScsbidInfoService01";
     public final static String WINBID_METHOD = "/getScsbidListSttusThng01";
